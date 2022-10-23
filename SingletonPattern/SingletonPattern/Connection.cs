5ef3413ac991b92f08b47c41bd931c9d9eb7b5ad
Onur BiLKE <64010601+bilkeonur@users.noreply.github.com>
2022-10-23 14:11:06 +0300

    Add files via upload

---
+++ SingletonPattern/SingletonPattern/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace SingletonPattern
+{
+    public class Connection
+    {
+        private static Connection connection = null;
+
+        private Connection()
+        {
+
+        }
+
+        public static Connection GetInstance()
+        {
+            if (connection == null)
+            {
+                return new Connection();
+            }
+            else
+            {
+                return connection;
+            }
+        }
+
+        public void OpenConnection()
+        {
+            Console.WriteLine("Bağlantı Açıldı");
+        }
+    }
+}
+
---
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connection connection1 = Connection.GetInstance();
+            connection1.OpenConnection();
+
+            Connection connection2 = Connection.GetInstance();
+            connection2.OpenConnection();
+        }
+    }
+}
+
